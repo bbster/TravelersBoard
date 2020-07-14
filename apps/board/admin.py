40bdd8dcdfdca920bbf735641f380816d01c3586
bbster <bbster12@naver.com>
2020-07-14 15:36:05 +0900

    python shell로 related연습겸 중첩댓글 출력을 위한 테스트

--- apps/board/admin.py
+++ apps/board/admin.py
@@ -4,17 +4,17 @@ from .models import Board, Post, Comment
 
 class BoardAdmin(admin.ModelAdmin):
     fields = ['unique_title']
-    list_display = ['unique_title', 'create_date', 'modify_date']
+    list_display = ['id', 'unique_title', 'create_date', 'modify_date']
 
 
 class PostAdmin(admin.ModelAdmin):
     fields = ['board', 'title', 'content', 'creator']
-    list_display = ['board', 'title', 'content', 'creator', 'create_date', 'modify_date']
+    list_display = ['id', 'board', 'title', 'content', 'creator', 'create_date', 'modify_date']
 
 
 class CommentAdmin(admin.ModelAdmin):
     fields = ['post', 'content', 'parent', 'creator']
-    list_display = ['post', 'content', 'creator', 'parent', 'create_date']
+    list_display = ['id', 'post', 'content', 'creator', 'parent', 'create_date']
 
 
 admin.site.register(Board, BoardAdmin)
